95705caa9edf35a75f357b078283a3f6acb0679c
ANJANI KUMARI <anjaniiit6@gmail.com>
2026-07-17 12:26:48 +0530

    Create appointment service package structure

---
+++ appointment-service/src/main/java/com/healthcare/appointment_service/AppointmentApplication.java
@@ -0,0 +1,13 @@
+package com.healthcare.appointment_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppointmentApplication.class, args);
+	}
+
+}
